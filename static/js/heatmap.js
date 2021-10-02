381d03049d9709bfa7a58a4d409feb667992f3db
LittleBanana313 <80846797+LittleBanana313@users.noreply.github.com>
2021-10-02 10:27:45 -0400

    Update heatmap.js

--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -7,26 +7,3 @@ var myMap = L.map("map", {
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
-
-var url = "https://data.sfgov.org/resource/cuks-n6tp.json?$limit=10000";
-
-d3.json(url).then(function(response) {
-
-  console.log(response);
-
-  var heatArray = [];
-
-  for (var i = 0; i < response.length; i++) {
-    var location = response[i].location;
-
-    if (location) {
-      heatArray.push([location.coordinates[1], location.coordinates[0]]);
-    }
-  }
-
-  var heat = L.heatLayer(heatArray, {
-    radius: 20,
-    blur: 35
-  }).addTo(myMap);
-
-});
