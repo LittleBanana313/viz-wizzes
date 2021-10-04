ceb350f7bfdaca97e6a8dd25271218df2b125dc4
LittleBanana313 <80846797+LittleBanana313@users.noreply.github.com>
2021-10-04 11:13:43 -0400

    Update heatmap.js

--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -1,4 +1,4 @@
-var myMap = L.map("map", {
+var mapid = L.map("mapid", {
   center: [37.7749, -122.4194],
   zoom: 13
 });
@@ -6,27 +6,62 @@ var myMap = L.map("map", {
 // Adding the tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
+}).addTo(mapid);
 
-var url = "https://data.sfgov.org/resource/cuks-n6tp.json?$limit=10000";
+// Connect crime database
+var link = '../../nyc_crime.csv'
+console.log(link)
 
-d3.json(url).then(function(response) {
+// changes color of a neighborhood based on the borough 
+function chooseColor(district) {
+  if (district == "Brooklyn") return "yellow";
+  else if (district == "Bronx") return "red";
+  else if (district == "Manhattan") return "orange";
+  else if (district == "Queens") return "green";
+  else if (district == "Staten Island") return "purple";
+  else return "black";
+}
 
-  console.log(response);
+// Get data
+d3.json(link).then(function(data) {
+  // Creating a Geo layer with the retrieved data
+  L.geoJson(data, {
+    // Styling each feature (in this case, a neighborhood)
+    style: function(feature) {
+      return {
+        color: "white",
+        // Call the chooseColor() function to decide which color to color our neighborhood. (The color is based on the borough.)
+        fillColor: chooseColor(feature.properties.district),
+        fillOpacity: 0.5,
+        weight: 1.5
+      };
+    },
+    // This is called on each feature.
+    onEachFeature: function(feature, layer) {
+      // Set the mouse events to change the map styling.
+      layer.on({
+        // When a user's mouse cursor touches a map feature, the mouseover event calls this function, which makes that feature's opacity change to 90% so that it stands out.
+        mouseover: function(event) {
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.9
+          });
+        },
+        // When the cursor no longer hovers over a map feature (that is, when the mouseout event occurs), the feature's opacity reverts back to 50%.
+        mouseout: function(event) {
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.5
+          });
+        },
+        // When a feature (neighborhood) is clicked, it enlarges to fit the screen.
+        click: function(event) {
+          myMap.fitBounds(event.target.getBounds());
+        }
+      });
+      // Giving each feature a popup with information that's relevant to it
+      layer.bindPopup("<h1>" + feature.properties.neighborhood + "</h1> <hr> <h2>" + feature.properties.borough + "</h2>");
 
-  var heatArray = [];
-
-  for (var i = 0; i < response.length; i++) {
-    var location = response[i].location;
-
-    if (location) {
-      heatArray.push([location.coordinates[1], location.coordinates[0]]);
     }
-  }
-
-  var heat = L.heatLayer(heatArray, {
-    radius: 20,
-    blur: 35
-  }).addTo(myMap);
-
+  }).addTo(mapid);
 });
