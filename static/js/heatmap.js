6e6a4d287f706f102956fb6002be97af840c821e
Mehdi Rahman <mailmehdi203@gmail.com>
2021-10-04 23:22:21 -0400

    adding working heatmap file

--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -1,9 +1,8 @@
-var mapid = L.map("mapid", {
+var mapid = L.map("map", {
   center: [40.7128, -74.0060],
-  zoom: 13
+  zoom: 12
 });
-6
-setTimeout(function(){ map.invalidateSize()}, 400);
+
 
 // Adding the tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -11,29 +10,34 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(mapid);
 
 // Connect crime database
-var data = '../../nyc_crime.json'
+var data = 'http://127.0.0.1:5000/db_call'
 console.log(data)
 
 
 d3.json(data).then(function(response) {
 
-  console.log(response);
+  // Create a new marker cluster group.
+  var markers = L.markerClusterGroup();
 
-  var heatArray = [];
-
+  // Loop through the data.
   for (var i = 0; i < response.length; i++) {
+
+    // Set the data location property to a variable.
     var location1 = response[i].latitude;
     var location2 = response[i].longitude;
-    var location = [location1, location2]
 
+    // Check for the location property.
     if (location) {
-      heatArray.push([location.coordinates[1], location.coordinates[0]]);
+
+      // Add a new marker to the cluster group, and bind a popup.
+      markers.addLayer(L.marker([location1, location2])
+        .bindPopup(response[i].ofns_desc));
     }
+
   }
 
-  var heat = L.heatLayer(heatArray, {
-    radius: 20,
-    blur: 35
-  }).addTo(mapid);
+  // Add our marker cluster layer to the map.
+  mapid.addLayer(markers);
 
 });
+
